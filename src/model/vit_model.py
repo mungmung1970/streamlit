5faf705a4e09f2f36862960b6c56a3e14c2b127d
오민경 <mungmung1970@gmail.com>
2026-01-07 18:00:24 +0900

    first commit

--- src/model/vit_model.py
+++ src/model/vit_model.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 
 @st.cache_resource
-def load_vit_model():
+def load_vit_model(model_name: str):
     from transformers import AutoImageProcessor, ViTForImageClassification
-    import torch
 
-    processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
-    model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+    processor = AutoImageProcessor.from_pretrained(model_name)
+    model = ViTForImageClassification.from_pretrained(model_name)
     model.eval()
+
     return processor, model
